3f4045e91a59f983b94628af996fd2802964841b
Widibuket <94840347+Widibuket@users.noreply.github.com>
2021-12-14 13:32:53 +0800

    Create footer.php

---
+++ admin/patrials/footer.php
@@ -0,0 +1,9 @@
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center"><a href="#">WIDI TEAM</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
